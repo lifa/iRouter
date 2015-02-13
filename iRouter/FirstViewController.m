4e564bac371e091223a5ac76020ff077dcbf5324
fanlifei <fanlifei@sogou-inc.com>
2015-02-13 18:12:09 +0800

    Initial Commit

---
+++ iRouter/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  iRouter
+//
+//  Created by fan on 2/13/15.
+//  Copyright (c) 2015 fanfan. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iRouterTests/iRouterTests.m
@@ -0,0 +1,40 @@
+//
+//  iRouterTests.m
+//  iRouterTests
+//
+//  Created by fan on 2/13/15.
+//  Copyright (c) 2015 fanfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iRouterTests : XCTestCase
+
+@end
+
+@implementation iRouterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
